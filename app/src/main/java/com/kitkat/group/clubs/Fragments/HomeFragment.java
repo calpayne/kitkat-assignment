bcf96de7fba3cc37d650a1f9e14496573b7ee946
GLENN BAKER <T7014713@live.tees.ac.uk>
2019-02-13 12:56:21 +0000

    Added navigation with functionality

---
+++ app/src/main/java/com/kitkat/group/clubs/Fragments/HomeFragment.java
@@ -0,0 +1,32 @@
+package com.kitkat.group.clubs.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kitkat.group.clubs.R;
+
+/**
+ * Created by Glenn on 13/02/2019.
+ */
+
+public class HomeFragment extends Fragment {
+
+    private static final String TAG = "HomeFragment";
+
+    public HomeFragment() {
+        // Empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView: started HomeFragment");
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return view;
+    }
+}
