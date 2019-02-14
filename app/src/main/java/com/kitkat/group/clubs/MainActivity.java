0e228dca0b6daee5e8ab7025f30cc777e2662e4f
Cal Payne <34384159+calpayne@users.noreply.github.com>
2019-02-14 13:04:46 +0000

    Added Login screen and functionality (#3)
    * Added logout functionality
    
    * Added no login for users already logged in
    
    * Login screen UI update
    
    * Login screen UI update
    
    * Login screen UI update
    
    * Changed app icon

--- app/src/main/java/com/kitkat/group/clubs/MainActivity.java
+++ app/src/main/java/com/kitkat/group/clubs/MainActivity.java
@@ -1,7 +1,14 @@
 package com.kitkat.group.clubs;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void logout(View view) {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
     }
 }
