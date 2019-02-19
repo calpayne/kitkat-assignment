b3fe846c38f68d9ec16efe9eece64a349853d5db
shakthi1109 <mail2shakthivel98@gmail.com>
2019-02-19 14:03:27 +0000

    Nfc feature updated

--- app/src/main/java/com/kitkat/group/clubs/HomeFragment.java
+++ app/src/main/java/com/kitkat/group/clubs/HomeFragment.java
@@ -1,11 +1,13 @@
 package com.kitkat.group.clubs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by Glenn on 13/02/2019.
@@ -14,6 +16,7 @@ import android.view.ViewGroup;
 public class HomeFragment extends Fragment {
 
     private static final String TAG = "HomeFragment";
+    public Button nfcbutton;
 
     public HomeFragment() {
         // Empty public constructor
@@ -24,6 +27,16 @@ public class HomeFragment extends Fragment {
         Log.d(TAG, "onCreateView: started HomeFragment");
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        nfcbutton = (Button) view.findViewById(R.id.nfcbutton);
+        nfcbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), SenderActivity.class);
+                //intent.putExtra("text",text);
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
