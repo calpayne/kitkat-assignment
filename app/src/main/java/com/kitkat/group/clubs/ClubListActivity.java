b9dc305c0c0ae7fca2adb987592dc34b723ccc69
calpayne <34384159+calpayne@users.noreply.github.com>
2019-02-24 18:30:35 +0000

    Basic layout for clubs list activity done

---
+++ app/src/main/java/com/kitkat/group/clubs/ClubListActivity.java
@@ -0,0 +1,13 @@
+package com.kitkat.group.clubs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClubListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_club_list);
+    }
+}
