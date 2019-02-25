310169ec6794f8efbedc4effa2415196ec71b6a0
calpayne <34384159+calpayne@users.noreply.github.com>
2019-02-25 15:43:58 +0000

    Listview now updates when a club is edited or deleted

--- app/src/main/java/com/kitkat/group/clubs/ClubListActivity.java
+++ app/src/main/java/com/kitkat/group/clubs/ClubListActivity.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 
 import com.google.firebase.database.ChildEventListener;
@@ -42,12 +43,30 @@ public class ClubListActivity extends AppCompatActivity {
 
             @Override
             public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Club data = dataSnapshot.getValue(Club.class);
+
+                for (Club c : clubs) {
+                    if (c.getClubID().equalsIgnoreCase(data.getClubID())) {
+                        c.setClubName(data.getClubName());
+                        break;
+                    }
+                }
+
                 listAdapter.notifyDataSetChanged();
             }
 
             @Override
             public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+                Club data = dataSnapshot.getValue(Club.class);
 
+                for (Club c : clubs) {
+                    if (c.getClubID().equalsIgnoreCase(data.getClubID())) {
+                        clubs.remove(c);
+                        break;
+                    }
+                }
+
+                listAdapter.notifyDataSetChanged();
             }
 
             @Override
