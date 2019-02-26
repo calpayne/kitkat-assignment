b9470baf7ea5ebb378185fc351958e23d2ca5158
Freezy705 <35915692+Freezy705@users.noreply.github.com>
2019-02-26 13:24:58 +0000

    Created activity for viewing club profiles (#10)
    * Added relevant activities & passed data between them.
    
    * Added read functionality for club details, issue with title.

--- app/src/main/java/com/kitkat/group/clubs/ClubsFragment.java
+++ app/src/main/java/com/kitkat/group/clubs/ClubsFragment.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.google.firebase.database.DatabaseReference;
+
 /**
  * Created by Glenn on 13/02/2019.
  */
@@ -35,6 +37,17 @@ public class ClubsFragment extends Fragment {
                 startActivity(intent);
             }
         });
+
+        Button btn2 = (Button) view.findViewById(R.id.btn_test_profile);
+        btn2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getActivity(), ViewClubActivity.class);
+                intent.putExtra("clubId","d57561cb-f66b-4263-88d8-800d5d84b341");
+                startActivity(intent);
+            }
+        });
+
 
         return view;
     }
---
+++ app/src/main/java/com/kitkat/group/clubs/ViewClubActivity.java
@@ -0,0 +1,82 @@
+package com.kitkat.group.clubs;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.kitkat.group.clubs.data.Club;
+
+public class ViewClubActivity extends AppCompatActivity {
+
+    private DatabaseReference db;
+    private Club club;
+    private static final String VTAG = "ViewClubActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_club);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        db = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference instance = db.child("clubs");
+
+        //DocumentReference docRef = db.collection("cities").document(getIntent().getStringExtra("clubId"));
+
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                DataSnapshot ds = dataSnapshot.child(getIntent().getStringExtra("clubId"));
+                    club = new Club(
+                            ds.child("clubName").getValue(String.class),
+                            ds.child("clubDescription").getValue(String.class),
+                            ds.child("clubOwner").getValue(String.class)
+                    );
+                System.out.println(ds.child("clubName").getValue(String.class));
+                System.out.println(ds.child("clubDescription").getValue(String.class));
+                System.out.println(ds.child("clubOwner").getValue(String.class));
+
+                if(club == null)
+                    System.out.println("Club instance is null.");
+                else
+                    System.out.print(club.toString());
+
+                //ImageView imageView = (ImageView) findViewById(R.id.clubImage);
+                //imageView.setImage();
+
+                TextView textView = (TextView) findViewById(R.id.textView);
+                textView.setText(club.getClubDescription());
+
+                FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+                fab.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Snackbar.make(view, club.getClubOwner(), Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                    }
+                });
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("Database error.");
+                Log.e(VTAG, "onCancelled", databaseError.toException());
+            }
+        };
+        instance.addListenerForSingleValueEvent(valueEventListener);
+        this.setTitle(instance.child(getIntent().getStringExtra("clubId")).child("clubName").getKey());
+    }
+
+
+}
