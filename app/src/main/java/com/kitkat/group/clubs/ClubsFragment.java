95760d062fa0049cd9664f2a5e565f34bf14a36c
glennmckimm <t7014713@live.tees.ac.uk>
2019-03-04 23:14:32 +0000

    Added QR Code Scanner class

--- app/src/main/java/com/kitkat/group/clubs/ClubsFragment.java
+++ app/src/main/java/com/kitkat/group/clubs/ClubsFragment.java
@@ -1,8 +1,11 @@
 package com.kitkat.group.clubs;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +19,9 @@ import android.widget.Button;
 public class ClubsFragment extends Fragment {
 
     private static final String TAG = "ClubsFragment";
+
+    public static final int REQUEST_CODE = 100;
+    public static final int PERMISSIONS_REQUESTED = 200;
 
     public ClubsFragment() {
         // Empty public constructor
@@ -43,6 +49,20 @@ public class ClubsFragment extends Fragment {
                 Intent intent = new Intent(getActivity(), ViewClubActivity.class);
                 intent.putExtra("clubId","d57561cb-f66b-4263-88d8-800d5d84b341");
                 startActivity(intent);
+            }
+        });
+
+        Button btn3 = (Button) view.findViewById(R.id.btn_scan_qr);
+        if (ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{android.Manifest.permission.CAMERA}, PERMISSIONS_REQUESTED);
+        }
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ScanQRCodeActivity.class);
+                startActivityForResult(intent, REQUEST_CODE);
             }
         });
 
---
+++ app/src/main/java/com/kitkat/group/clubs/ScanQRCodeActivity.java
@@ -0,0 +1,100 @@
+package com.kitkat.group.clubs;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Vibrator;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+public class ScanQRCodeActivity extends AppCompatActivity {
+
+    SurfaceView surfaceView;
+    CameraSource cameraSource;
+    TextView textView;
+    BarcodeDetector barcodeDetector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_qrcode);
+
+        surfaceView = (SurfaceView) findViewById(R.id.camera_preview);
+        textView = (TextView) findViewById(R.id.textView);
+
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.QR_CODE).build();
+
+        cameraSource = new CameraSource.Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(640, 480).build();
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(),
+                        android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                try {
+                    cameraSource.start(surfaceHolder);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                SparseArray<Barcode> qrCode = detections.getDetectedItems();
+
+                if (qrCode.size() != 0) {
+                    textView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            /**
+                             *   Vibrator vibrator = (Vibrator)getApplicationContext()
+                             *          .getSystemService(Context.VIBRATOR_SERVICE);
+                             *   vibrator.vibrate(1000); -->
+                             */
+                            textView.setText(qrCode.valueAt(0).displayValue);
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
