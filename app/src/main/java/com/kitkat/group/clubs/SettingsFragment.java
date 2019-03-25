df6bde5ca8f40a593190cf5ab262ebea065557db
calpayne <34384159+calpayne@users.noreply.github.com>
2019-03-25 21:44:57 +0000

    Users can now upload/change profile pictures

--- app/src/main/java/com/kitkat/group/clubs/SettingsFragment.java
+++ app/src/main/java/com/kitkat/group/clubs/SettingsFragment.java
@@ -2,6 +2,7 @@ package com.kitkat.group.clubs;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -13,8 +14,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.kitkat.group.clubs.nfc.SenderActivity;
+
+import static android.app.Activity.RESULT_OK;
+
 /**
  * Created by Glenn on 17/02/2019.
  * (Do not remove)
@@ -22,7 +32,9 @@ import com.kitkat.group.clubs.nfc.SenderActivity;
 
 public class SettingsFragment extends Fragment {
 
+    private StorageReference storageRef;
     private static final String TAG = "SettingsFragment";
+    private static final int GALLERY_INTENT = 2;
 
     public SettingsFragment() {
         // Empty public constructor
@@ -32,6 +44,36 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         Log.d(TAG, "onCreateView: started SettingsFragment");
 
-        return inflater.inflate(R.layout.fragment_user_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_user_settings, container, false);
+
+        storageRef = FirebaseStorage.getInstance().getReference();
+
+        view.findViewById(R.id.change_profile_picture).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/png");
+
+                startActivityForResult(intent, GALLERY_INTENT);
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_INTENT && resultCode == RESULT_OK) {
+            Uri image = data.getData();
+            StorageReference filePath = storageRef.child("member-avatars").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+            filePath.putFile(image).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(getActivity(), "Profile picture changed.", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
