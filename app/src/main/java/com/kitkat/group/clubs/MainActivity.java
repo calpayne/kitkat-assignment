67bff545e1464f2c6374b50b462af77404f1db2e
glennmckimm <t7014713@live.tees.ac.uk>
2019-04-01 22:27:07 +0100

    Added a Navigation Drawer and removed BottomNavigationView

--- app/src/main/java/com/kitkat/group/clubs/MainActivity.java
+++ app/src/main/java/com/kitkat/group/clubs/MainActivity.java
@@ -4,32 +4,43 @@ import android.app.PendingIntent;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 
 import android.content.Intent;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.kitkat.group.clubs.auth.LoginActivity;
 import com.kitkat.group.clubs.clubs.ClubsFragment;
+import com.kitkat.group.clubs.data.ClubUser;
+import com.kitkat.group.clubs.data.Member;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = "MainActivity";
-
-    final Fragment homeFragment = new HomeFragment();
-    final Fragment clubsFragment = new ClubsFragment();
-    final Fragment userFragment = new UserFragment();
-    final FragmentManager fm = getSupportFragmentManager();
-    Fragment active = homeFragment;
     private NfcAdapter nfcAdapter;
+    private TextView profileUsername;
+    private DrawerLayout drawer;
+    private FirebaseUser fuser;
+    private DatabaseReference databaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +49,54 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d(TAG, "onCreate: Started MainActivity.");
 
-        BottomNavigationView navigation = findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        //profileUsername = findViewById(R.id.profile_username);
+        //setProfileUsername();
 
-        fm.beginTransaction().add(R.id.fragment_container, userFragment, "3").hide(userFragment).commit();
-        fm.beginTransaction().add(R.id.fragment_container, clubsFragment, "2").hide(clubsFragment).commit();
-        fm.beginTransaction().add(R.id.fragment_container, homeFragment, "1").commit();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.container);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null ){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+   }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_clubs:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ClubsFragment()).commit();
+                Toast.makeText(this, "Clubs", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_user:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new UserFragment()).commit();
+                Toast.makeText(this, "User", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.isDrawerOpen(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     public void onResume() {
@@ -63,28 +116,6 @@ public class MainActivity extends AppCompatActivity {
             // drop NFC events
         }
     }
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch(item.getItemId()) {
-                case R.id.navigation_home:
-                    fm.beginTransaction().hide(active).show(homeFragment).commit();
-                    active = homeFragment;
-                    return true;
-                case R.id.navigation_clubs:
-                    fm.beginTransaction().hide(active).show(clubsFragment).commit();
-                    active = clubsFragment;
-                    return true;
-                case R.id.navigation_user:
-                    fm.beginTransaction().hide(active).show(userFragment).commit();
-                    active = userFragment;
-                    return true;
-            }
-            return false;
-        }
-    };
   
     public void logout(View view) {
         AuthUI.getInstance()
